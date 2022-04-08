1078dcdc0ace635d0bc33a9235be04575d52125b
ecekokyar <hecekokyar@gmail.com>
2022-04-08 22:35:52 +0300

    ilk ödev tamamlandı

---
+++ Odev1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faktöriyel Hesaplama
+           try
+           {
+                Console.WriteLine("Bir sayı giriniz: ");
+                int number=Convert.ToInt32(Console.ReadLine());
+    
+                int factorial=1;
+                
+                for (int i = 1; i <= number; i++)
+                {
+                    factorial*=i;                
+                }
+                Console.WriteLine($"{number}!={factorial}");
+           }
+           catch (FormatException)
+           {
+              Console.WriteLine("Lütfen bir sayı giriniz.");
+           }
+        }
+    }
+}
