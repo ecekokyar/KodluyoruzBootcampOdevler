90d523a0cdef33ab3f65cc656fa02f9aceb01956
ecekokyar <hecekokyar@gmail.com>
2022-04-14 21:30:55 +0300

    Ödev 3 tamamlandı

---
+++ Hafta1/Odev3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Odev3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1'den 10.000'e kadar tüm asal sayılar
+
+            int number=1;
+            bool status =true;
+
+            while (number<10000)
+            {
+                number++;
+                for (int i = 2; i <number; i++)
+                {
+                    if (number%i==0)
+                    {
+                        status=false;
+                    }
+
+                }
+                if (status==true)
+                {
+                    Console.WriteLine(number);
+                }
+                status=true;
+            }
+            
+           
+           
+
+            
+        }
+    }
+}
