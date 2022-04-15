31a2ff346b3089c54175eddb7228c5f4f1473359
ecekokyar <hecekokyar@gmail.com>
2022-04-15 18:29:38 +0300

    2.haftanın 1.ödevi tamamlandı

---
+++ Hafta2/Odev1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Liste içinde birden fazla olan öğeleri bir kez yazdırma
+
+            List<string> cities = new List<string>() {"Ankara","Ankara","Ankara","Sinop","İstanbul","İstanbul","Eskişehir","Ankara"};
+
+            List<string> newColection=cities.Distinct().ToList();
+            // C# Linq Distinct Metodu:
+            // Koleksiyonda bulunan verilerin tekrarlayan kayıtların tekrarlanmamasını sağlar.
+
+            foreach (var city in newColection)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}
